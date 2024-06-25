3fbd5e82000684983f9cd8ecde38d03316cc1428
goott3 <xogud2580@naver.com>
2024-06-25 18:21:02 +0900

    채팅 기능 추가중

---
+++ src/main/java/com/artissue/controller/ChatController.java
@@ -0,0 +1,12 @@
+package com.artissue.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController {
+
+}
