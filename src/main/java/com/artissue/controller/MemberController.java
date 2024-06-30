c3dd48b41cbcbc5bc375670edbb21a775ad92c29
BSH <qotngus31179@naver.com>
2024-06-30 18:53:41 +0900

    coolsms 의존성 추가 문자전송 개발중

--- src/main/java/com/artissue/controller/MemberController.java
+++ src/main/java/com/artissue/controller/MemberController.java
@@ -3,10 +3,17 @@ package com.artissue.controller;
 import com.artissue.model.MemberDTO;
 import com.artissue.model.MemberMapper;
 import com.artissue.service.JoinService;
+import com.artissue.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 public class MemberController {
@@ -15,21 +22,24 @@ public class MemberController {
     private MemberMapper memberMapper;
 
     @Autowired
+    private MessageService messageService;
+
+    @Autowired
     private JoinService joinService;
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
 
         return "login";
     }
 
     @GetMapping("/join")
-    public String join(){
+    public String join() {
         return "memberJoin";
     }
 
     @PostMapping("/joinProc")
-    public String joinProc(MemberDTO memberDTO){
+    public String joinProc(MemberDTO memberDTO) {
 
         joinService.joinProcess(memberDTO);
 
@@ -37,8 +47,31 @@ public class MemberController {
     }
 
     @GetMapping("/findbyId")
-    public String findId(){
+    public String findId() {
         return "findbyId";
+    }
+
+    @PostMapping("/sendVerificationCode")
+    @ResponseBody
+    public ResponseEntity<String> sendVerificationCode(@RequestParam String phone) {
+        try {
+
+            String verificationCode = generateVerificationCode();
+            messageService.sendOne(phone, verificationCode);
+
+
+            return ResponseEntity.ok("{\"success\": true}");
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"success\": false}");
+        }
+    }
+
+    private String generateVerificationCode() {
+        Random random = new Random();
+        int code = 1000 + random.nextInt(9000);
+        return String.valueOf(code);
     }
 
 }
