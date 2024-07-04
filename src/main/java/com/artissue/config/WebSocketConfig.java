280f9f042ffad890d8e5ff1b49f7d0f5cb8cc949
goott3 <xogud2580@naver.com>
2024-07-04 10:52:56 +0900

    채팅 기능 추가중

--- src/main/java/com/artissue/config/WebSocketConfig.java
+++ src/main/java/com/artissue/config/WebSocketConfig.java
@@ -1,29 +1,25 @@
 package com.artissue.config;
 
 
+import com.artissue.service.WebSocketChatHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+    private final WebSocketChatHandler webSocketChatHandler;
+
+    public WebSocketConfig(WebSocketChatHandler webSocketChatHandler) {
+        this.webSocketChatHandler = webSocketChatHandler;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // 테스트할 때만 setAllowedOrigins("*"); 허용
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatHandler, "/ws/chat/*").setAllowedOrigins("*");
     }
+}
 
     
-}
+
