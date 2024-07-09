17634c960155062d67f9f0a60b715f5a77943181
baesoosoo <qotngus31179@naver.com>
2024-07-09 09:49:42 +0900

    소셜로그인 네이버로그인 완료

---
+++ src/main/java/com/artissue/model/NaverResponse.java
@@ -0,0 +1,46 @@
+package com.artissue.model;
+
+import java.util.Map;
+
+public class NaverResponse implements OAuth2Response{
+
+    private final Map<String,Object> attribute;
+
+    public NaverResponse(Map<String,Object> attributes){
+
+        this.attribute = (Map<String,Object>)attributes.get("response");
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
+
+    @Override
+    public String getJender() {
+        return attribute.get("jender").toString();
+    }
+
+    @Override
+    public String getNumber() {
+        return attribute.get("number").toString();
+    }
+}
