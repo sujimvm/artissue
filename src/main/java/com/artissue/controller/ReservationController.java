d0b494a81b609e283a0e0c64d1eee4b8ba0bc019
suji <sujimvm@naver.com>
2024-07-09 16:57:35 +0900

    예매하기 값 insert (#5) - suji

--- src/main/java/com/artissue/controller/ReservationController.java
+++ src/main/java/com/artissue/controller/ReservationController.java
@@ -1,13 +1,14 @@
 package com.artissue.controller;
 
-import com.artissue.model.ExhibitionDTO;
-import com.artissue.model.ExhibitionMapper;
-import com.artissue.model.ReservationDTO;
-import com.artissue.model.ReservationMapper;
+import com.artissue.model.*;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/reserve")
@@ -28,16 +29,50 @@ public class ReservationController {
     }
 
     @PostMapping("/sendReserve")
-    public void reservation(ReservationDTO reserveDTO) {
+    @ResponseBody
+    public void reservation(ReservationDTO reserveDTO, HttpSession session) {
+        int addResult = 0;
+
         // 값 insert
-        System.out.println(reserveDTO);
+        String[] optionStr = reserveDTO.getReservation_option().split(",");
+        String[] countStr = reserveDTO.getReservation_count_str().split(",");
+        String[] priceStr = reserveDTO.getReservation_price_str().split(",");
+
+        ReservationDTO insertDTO = new ReservationDTO();
+
+        MemberDTO memberDTO = (MemberDTO)session.getAttribute("mDTO");
+
+        Date today = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
+        System.out.println("포맷 지정 후: " + dateFormat.format(today));
+
+        String Reservation_id = memberDTO.getMember_id().substring(0,2).toUpperCase();
+        Reservation_id += dateFormat.format(today);
+
+        System.out.println(Reservation_id+" > ?");
+
+        insertDTO.setExhibition_key(reserveDTO.getExhibition_key());
+        insertDTO.setMember_key(memberDTO.getMember_key());
+        insertDTO.setReservation_id(Reservation_id);
+
+        for (int i = 0; i < optionStr.length; i++) {
+            if(Integer.parseInt(priceStr[i]) > 0){
+                insertDTO.setReservation_option(optionStr[i]);
+                insertDTO.setReservation_count(Integer.parseInt(countStr[i].split("/")[0]));
+                insertDTO.setReservation_price(Integer.parseInt(priceStr[i]));
+
+                addResult = reservationMapper.addReservation(insertDTO);
+            }
+        }
+
+        // 결제 페이지 이동 -> 결제
 
 
-        // QR 생성 및 예매 확인 페이지 구현
+        // QR 생성
 
         // 예매내역 및 예약 확인 페이지 링크 문자 발송
 
-        //예약확인 페이지로 이동
+        //예매 확인 페이지 구현 및 페이지로 이동
 
     }
 }
