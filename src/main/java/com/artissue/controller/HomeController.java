92be5e0d7acc4b3d1a3f19a4673cba2ddb346930
baesoosoo <qotngus31179@naver.com>
2024-07-10 10:00:49 +0900

    시큐리티 수정,회원가입 폼수정,네이버설정수정

--- src/main/java/com/artissue/controller/HomeController.java
+++ src/main/java/com/artissue/controller/HomeController.java
@@ -20,9 +20,36 @@ public class HomeController {
     @Autowired
     MemberMapper memberMapper;
 
-    @GetMapping("/")
-    public String main(){
-        return "index";
+   @GetMapping("/")
+    public String main(HttpSession session,Model model){
+
+       // 현재 인증된 사용자의 정보 가져오기
+       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+       // 사용자의 첫 번째 권한 가져오기
+       String role = auth.getAuthorities().iterator().next().getAuthority();
+       String id = auth.getName();
+
+       System.out.println("role>>> " + role);
+       System.out.println("id>>> " + id);
+
+       if (role.equals("ROLE_USER")) {
+           session.setAttribute("role", role);
+
+           MemberDTO memberDTO = memberMapper.findUsername(id);
+           session.setAttribute("mDTO", memberDTO);
+       } else if (role.equals("ROLE_COMPANY")) {
+           session.setAttribute("role", role);
+
+           MemberDTO companyDTO = memberMapper.findUsername(id);
+           session.setAttribute("cDTO", companyDTO);
+       }
+
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+       String username = authentication.getName();
+       model.addAttribute("username", username);
+
+       return "index";
     }
 
     @GetMapping("/qr")
